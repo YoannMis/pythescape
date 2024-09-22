433a7bfdcfe168e23d5500abdba359dda55d0608
YoannMis <ymisericordia@gmx.com>
2024-09-22 17:46:16 +0200

    add new class IntroWindow

--- app.py
+++ app.py
@@ -1,5 +1,76 @@
 from castle import *
 
+
+class IntroWindow(Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Pythescape")  # Titre de la fenêtre principale
+        self._window_width = 600  # Largeur de la fenêtre
+        self._window_height = 600  # Hauteur de la fenêtre
+        self._screen_width = self.winfo_screenwidth()  # Largeur de l'écran où est lancée l'appli
+        self._screen_height = self.winfo_screenheight()  # Hauteur de l'écran où est lancée l'appli
+        # Calcul de l'abscisse du centre de l'écran
+        self._center_x = int(self._screen_width / 2 - self._window_width / 2)
+        # Calcul de l'ordonnée du centre de l'écran
+        self._center_y = int(self._screen_height / 2 - self._window_height / 2)
+        # Paramètres de la fenêtre principale
+        self.geometry(f"{self._window_width}x{self._window_height}+{self._center_x}+{self._center_y}")
+        self.resizable(False, False)  # Empêcher de pouvoir modifier la taille de la fenêtre
+
+        # Ajout d'un widget Label afin d'afficher les règles du jeu
+        self._game_rules = Label(self,
+                                 text=MESSAGES["rules"],
+                                 font=("Helvetica", 14),
+                                 justify=CENTER,
+                                 wraplength=500,
+                                 )
+        self._game_rules.pack(pady=30)  # Empaquetage du widget dans la fenêtre
+
+        # Ajout d'un widget Button afin d'avoir un bouton cliquable permettant de lancer le jeu
+        self._start_game_btn = ttk.Button(self, text="Ok", command=self._start)
+        self._start_game_btn.pack(pady=30)  # Empaquetage du widget dans la fenêtre
+
+    def _start(self) -> None:
+        """Permet de lancer la commande lors de l'utilisation du bouton d'action.
+        Lance le jeu en cliquant sur le bouton.
+
+        Returns: None
+        """
+        self.destroy()  # Ferme la fenêtre principale avec les règles du jeu
+        Rules()()  # Lance la fenêtre Rules indiquant les règles du jeu
+
+    def __call__(self) -> None:
+        self.mainloop()
+
+
+# Class Rules pour faire apparaître dans une fenêtre au démarrage du jeu les règles du jeu
+class Rules(IntroWindow):
+    def __init__(self) -> None:
+        super().__init__()  # Créer une instance de la class Tk avec une fenêtre principale
+        # Ajout d'un widget Label afin d'afficher les règles du jeu
+        self._game_rules = Label(self,
+                                 text=MESSAGES["rules"],
+                                 font=("Helvetica", 14),
+                                 justify=CENTER,
+                                 wraplength=500,
+                                 )
+        self._game_rules.pack(pady=30)  # Empaquetage du widget dans la fenêtre
+
+        # Ajout d'un widget Button afin d'avoir un bouton cliquable permettant de lancer le jeu
+        self._start_game_btn = ttk.Button(self, text="Ok", command=self._start_game)
+        self._start_game_btn.pack(pady=30)  # Empaquetage du widget dans la fenêtre
+
+    def _start_game(self) -> None:
+        """Permet de lancer la commande lors de l'utilisation du bouton d'action.
+        Lance le jeu en cliquant sur le bouton.
+
+        Returns: None
+        """
+        self.destroy()  # Ferme la fenêtre principale avec les règles du jeu
+        Game()()  # Lance la fenêtre Turtle contenant le jeu
+
+
+
 # Class Game permettant de créer la dynamique du jeu afin de le lancer
 class Game:
     def __init__(self) -> None:
@@ -24,47 +95,5 @@ class Game:
         self._main_loop()
 
 
-# Class Rules pour faire apparaître dans une fenêtre au démarrage du jeu les règles du jeu
-class Rules(Tk):
-    def __init__(self) -> None:
-        super().__init__()  # Créer une instance de la class Tk avec une fenêtre principale
-        self.title("Règles du Jeu Pythescape")  # Titre de la fenêtre principale
-        self._window_width = 600  # Largeur de la fenêtre
-        self._window_height = 600  # Hauteur de la fenêtre
-        self._screen_width = self.winfo_screenwidth()  # Largeur de l'écran où est lancée l'appli
-        self._screen_height = self.winfo_screenheight()  # Hauteur de l'écran où est lancée l'appli
-        self._center_x = int(
-            self._screen_width / 2 - self._window_width / 2)  # Calcul de l'abscisse du centre de l'écran
-        self._center_y = int(
-            self._screen_height / 2 - self._window_height / 2)  # Calcul de l'ordonnée du centre de l'écran
-        # Paramètres de la fenêtre principale
-        self.geometry(f"{self._window_width}x{self._window_height}+{self._center_x}+{self._center_y}")
-        self.resizable(False, False)  # Empêcher de pouvoir modifier la taille de la fenêtre
-
-        # Ajout d'un widget Label afin d'afficher les règles du jeu
-        self._game_rules = Label(self,
-                                 text=MESSAGES["rules"],
-                                 font=("Helvetica", 14),
-                                 justify=CENTER,
-                                 wraplength=500,
-                                 )
-        self._game_rules.pack(pady=30)  # Empaquetage du widget dans la fenêtre
-
-        # Ajout d'un widget Button afin d'avoir un bouton cliquable permettant de lancer le jeu
-        self._start_game_btn = ttk.Button(self, text="Ok", command=self._start_game)
-        self._start_game_btn.pack(pady=30)  # Empaquetage du widget dans la fenêtre
-
-    def _start_game(self) -> None:
-        """Permet de lancer la commande lors l'utilisation du bouton d'action.
-        Lance le jeu en cliquant sur le bouton.
-
-        Returns: None
-        """
-        self.destroy()  # Ferme la fenêtre principale avec les règles du jeu
-        Game()()  # Lance la fenêtre Turtle contenant le jeu
-
-    def __call__(self) -> None:
-        self.mainloop()
-
 if __name__ == '__main__':
-    Rules()()
+    IntroWindow()()
